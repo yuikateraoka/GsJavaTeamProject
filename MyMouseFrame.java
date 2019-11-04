c2b7a6f19048fbd9dad55112b71fda154707f434
YuikaTeraoka <yuikasunmoon@gmail.com>
2019-11-04 13:24:01 +0900

    MyMouseFrame 작성했습니다.

---
+++ MyMouseFrame.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ * MyMouseFrame 클래스 안에 
+ * 
+ * @author (20118315053 테라오카유이카키) 
+ * @version (2019.11.04)
+ */
+public class MyMouseFrame  extends JFrame
+{
+    public MyMouseFrame(){
+        setTitle("MouseListener & MouseMotionListener");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300,300);
+        this.setVisible(true);
+
+        JPanel jp = new JPanel();
+        this.add(jp);
+        jp.setFocusable(true);
+        setVisible(true);
+
+    }
+}
